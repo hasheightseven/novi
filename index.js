7983cd3a65af502760b58cc4f94f76aa9db0ae32
#EightSeven <hasheightseven@gmail.com>
2025-02-07 12:08:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./src/App.js";
+//import reportWebVitals from "./reportWebVitals";
 
-const root = document.getElementByClassName("novi-main");
+const root = ReactDOM.createRoot(document.getElementById("novi-main"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-root.render(<App />);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
